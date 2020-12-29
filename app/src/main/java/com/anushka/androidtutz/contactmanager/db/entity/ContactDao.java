a2b93a06502000406b69c831a8be569b1b20a1a9
MKemp <mkemp@accu-time.com>
2020-12-29 17:29:31 -0500

    Use ContactDao with room annotations instead of DatabaseHelper with boilerplate code

---
+++ app/src/main/java/com/anushka/androidtutz/contactmanager/db/entity/ContactDao.java
@@ -0,0 +1,28 @@
+package com.anushka.androidtutz.contactmanager.db.entity;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+@Dao
+interface ContactDao
+{
+    // Primary key is of type long
+    @Insert
+    public long addContact(Contact contact);
+    
+    @Update
+    public void updateContact(Contact contact);
+    
+    @Update
+    public void deleteContact(Contact contact);
+    
+    @Query("SELECT * FROM contacts")
+    public List<Contact> getContacts(Contact contact);
+    
+    @Query("SELECT * FROM contacts WHERE contact_id == :contactId")
+    public Contact getContacts(long contactId);
+}
